9ac3e3b80525ffa628977940de4a0b1a31bef5e3
Injecting Inc <opensource@injecting.ai>
2026-09-10 15:05:15 -0700

    Validate PPTX style sources before cascading overrides

--- go/pptxpatch/native_extract.go
+++ go/pptxpatch/native_extract.go
@@ -1892,7 +1892,7 @@ func (extractor *nativeExtractor) extractNativeParagraphs(txBody *nativeXMLNode,
 	if txBody == nil {
 		return []NativeParagraph{}, nil
 	}
-	resolvedBody, styleErr := resolveNativeLocalTextStyles(txBody, dialect)
+	resolvedBody, styleErr := resolveNativeLocalTextStyles(txBody, dialect, extractor.theme)
 	if styleErr != nil {
 		return nil, styleErr
 	}
